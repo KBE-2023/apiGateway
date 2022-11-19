b7e73b612a87c71c03a62b7dfde9e3b426a58164
unknown <ductrungcsp@gmail.com>
2022-11-19 12:02:50 +0100

    add new project

---
+++ apiGateway/src/test/java/com/kbe2223/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbe2223.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
